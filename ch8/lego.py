b40c8e84e0798829a0e7dc26e0ffebabbd2df2c9
zhao <1142749677@qq.com>
2015-08-09 19:35:01 +0800

    linear regression

--- ch8/lego.py
+++ ch8/lego.py
@@ -38,4 +38,42 @@ def setDataCollect():
 	scrapePage('setHtml/lego10189.html','out.txt', 2008, 5922, 299.99)
 	scrapePage('setHtml/lego10196.html','out.txt', 2009, 3263, 249.99)
 
-setDataCollect()
+def crossValidation(xArr,yArr,numVal=10):
+	m=len(yArr)
+	indexList=range(m)
+	errorMat=zeros((numVal,30))
+	for i in range(numVal):
+		trainX=[];trainY=[]
+		testX=[];testY=[]
+		random.shuffle(indexList)
+		for j in range(m):
+			if j<m*0.9:
+				trainX.append(xArr[indexList[j]])
+				trainY.append(yArr[indexList[j]])
+			else:
+				testX.append(xArr[indexList[j]])
+				testY.append(yArr[indexList[j]])
+		wMat=ridgeTest(trainX,trainY)
+		print wMat
+		for k in range(30):
+			matTestX=mat(testX);matTrainX=mat(trainX)
+			meanTrain=mean(matTrainX,0)
+			varTrain=var(matTrainX,0)
+			matTestX=(matTestX-meanTrain)/varTrain
+			yEst=matTestX*mat(wMat[k,:]).T+mean(trainY)
+			errorMat[i,k]=rssError(yEst.T.A,array(testY))
+	meanErrors=mean(errorMat,0)
+	minMean=float(min(meanErrors))
+	bestWeights=wMat[nonzero(meanErrors==minMean)]
+	print bestWeights
+	xMat=mat(xArr);yMat=mat(yArr).T
+	meanX=mean(xMat,0);varX=var(xMat,0)
+	unReg=bestWeights/varX
+	print "the best model from ridge regression is:\n",unReg
+	print "with constant term:",-1*sum(multiply(meanX,unReg))+mean(yMat)
+
+# setDataCollect()
+data,label=loadDataSet('out.txt')
+crossValidation(data,label)
+ridgeWeights=ridgeTest(data,label)
+# print ridgeWeights
