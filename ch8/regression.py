46123967a5ac51d3c210466f8ba6640dc6eae70e
zhao <1142749677@qq.com>
2015-08-09 14:18:21 +0800

    linear regression

--- ch8/regression.py
+++ ch8/regression.py
@@ -71,6 +71,60 @@ def drawLwlr(xArr,yArr,yHat):
 def rssError(yArr,yHatArr):
 	return ((yArr-yHatArr)**2).sum()
 
+def ridgeRegres(xMat,yMat,lam=0.2):
+	# xMat=mat(xArr);yMat=mat(yArr).T
+	xTx=xMat.T*xMat
+	denom=xTx+eye(shape(xMat)[1])*lam
+	if linalg.det(denom)==0:
+		print "The matrix can not inverse"
+		return
+	ws=denom.I*(xMat.T*yMat)
+	return ws
+
+def ridgeTest(xArr,yArr):
+	xMat=mat(xArr);yMat=mat(yArr).T
+	yMean=mean(yMat,0)
+	yMat=yMat-yMean
+	xMeans=mean(xMat,0)
+	xVar=var(xMat,0)
+	xMat=(xMat-xMeans)/xVar
+	numTestPts=30
+	wMat=zeros((numTestPts,shape(xMat)[1]))
+	for i in range(numTestPts):
+		ws=ridgeRegres(xMat,yMat,exp(i-10))
+		wMat[i,:]=ws.T
+	return wMat
+
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+	xMat=mat(xArr);yMat=mat(yArr).T
+	yMean=mean(yMat,0)
+	yMat=yMat-yMean
+	#xMat=regularize(xMat)
+	xMeans=mean(xMat,0)
+	xVar=var(xMat,0)
+	xMat=(xMat-xMeans)/xVar
+	m,n=shape(xMat)
+	returnMat=zeros((numIt,n))
+	ws=zeros((n,1));wsTest=ws.copy();wsMax=ws.copy
+	for i in range(numIt):
+		print ws.T
+		lowestError=inf
+		for j in range(n):
+			for sign in [-1,1]:
+				wsTest=ws.copy()
+				wsTest[j]+=eps*sign
+				yTest=xMat*wsTest
+				rssE=rssError(yMat.A,yTest.A)
+				# print rssE,lowestError,wsTest.T
+				if rssE<lowestError:
+					lowestError=rssE
+					wsMax=wsTest
+				# print wsMax.T
+		ws=wsMax.copy()
+		returnMat[i,:]=ws.T
+	return returnMat
+
+
 ##logistic regression
 # data,label=loadDataSet('ex0.txt')
 # print data
@@ -89,3 +143,29 @@ def rssError(yArr,yHatArr):
 # yHat=lwlrTest(data,data,label,0.01)
 
 # drawLwlr(data,label,yHat)
+
+##ridge regression 
+# data,label=loadDataSet('abalone.txt')
+# ridgeWeights=ridgeTest(data,label)
+# print ridgeWeights
+# fig=plt.figure()
+# ax=fig.add_subplot(111)
+# ax.plot(ridgeWeights)
+# plt.show()
+
+##stagewise
+# data,label=loadDataSet('abalone.txt')
+# a=stageWise(data,label,0.001,5000)
+# fig=plt.figure()
+# ax=fig.add_subplot(111)
+# ax.plot(a)
+# plt.show()
+
+# xMat=mat(data);yMat=mat(label).T
+# yMean=mean(yMat,0)
+# yMat=yMat-yMean
+# xMeans=mean(xMat,0)
+# xVar=var(xMat,0)
+# xMat=(xMat-xMeans)/xVar
+# weights=standRegres(xMat,yMat.T)
+# print weights
